c8976d62cde613adfef32843a2ddd46437f6c34f
alie-nel <natalielam243@gmail.com>
2024-11-16 17:04:22 +1100

    fix env issues

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOM,
-  projectId: process.env.PROJ_ID,
-  storageBucket: process.env.STRGE_BUCK,
-  messagingSenderId: process.env.MESS_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEAS_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOM,
+  projectId: process.env.REACT_APP_PROJ_ID,
+  storageBucket: process.env.REACT_APP_STRGE_BUCK,
+  messagingSenderId: process.env.REACT_APP_MESS_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEAS_ID
 };
 
 const app = initializeApp(firebaseConfig);
