3b0679eb5622b9b01f2c9033e2567ef4540b893e
alie-nel <natalielam243@gmail.com>
2024-11-17 17:38:25 +1100

    implement logic to add user data to database

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import "./Login.css";
 import { useState } from "react";
-import { auth, googleProvider } from "../../config/firebase";
+import { auth, googleProvider, db } from "../../config/firebase";
+import { setDoc, doc, getDoc } from "firebase/firestore";
 import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
@@ -12,27 +13,53 @@ const Login = ({ setIsAuth }) => {
 
   const login = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password).then((result) => {
-        localStorage.setItem("isAuth", true);
-        setIsAuth(true);
-        redirect("/");
-      });
+      const result = await createUserWithEmailAndPassword(auth, email, password);
+      const user = result.user;
+      const userRef = doc(db, "profiles", user.uid);
+      const userSnap = await getDoc(userRef);
+
+      if (!userSnap.exists()) {
+        await setDoc(userRef, {
+          username: user.email.split("@")[0],
+          posts: [],
+          likes: [], 
+          starred: [],
+        });
+      }
+
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      redirect("/");
+
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   const googleLogin = async () => {
     try {
-      await signInWithPopup(auth, googleProvider).then((result) => {
-        localStorage.setItem("isAuth", true);
-        setIsAuth(true);
-        redirect("/");
-      });
+      const result = await signInWithPopup(auth, googleProvider);
+      const user = result.user;
+      const userRef = doc(db, "profiles", user.uid); // Use uid as the document ID
+      const userSnap = await getDoc(userRef);
+
+      if (!userSnap.exists()) {
+        await setDoc(userRef, {
+          username: user.displayName,
+          posts: [],
+          likes: [], 
+          starred: [],
+        });
+      }
+
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      redirect("/");
+
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
   
   return (
     <>
